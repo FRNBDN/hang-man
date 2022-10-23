ff46d6cef06a55336b03a80b15d49ddb9b6499f7
FRNBDN <francesco.badini94@gmail.com>
2022-10-23 05:47:00 +0000

    add win and loss functions and update print style

--- run.py
+++ run.py
@@ -41,6 +41,16 @@ def game_start():
     """
     words = hangmanwordbank.words
     word = random.choice(words)
+    print(word)
+
+    print(hangmanwordbank.HANGMANPICS[0])
+    print('=================')
+    print('Progress:')
+    print(len(word)*'_ ')
+    print('=================')
+    print("Previous Guesses:")
+    print("None")
+    print('=================')
     # returns the randomised word
     # to the game_running function
     return word
@@ -55,28 +65,28 @@ def game_running():
     the game to end.
     """
     word_to_be_guessed = game_start()
-    print(word_to_be_guessed)
     # optional: pass through difficulty setting
-    guessed = []
-    tot_wrong = len(hangmanwordbank.HANGMANPICS)
-    while tot_wrong > 0:
+    guessed = ""
+    tot_wrong = 0
+    while tot_wrong < len(hangmanwordbank.HANGMANPICS):
         # Prompts the players guess
         usr_guess = input('Guess a letter\n')
-        # add validation step
         if input_validation(usr_guess, 'letter'):
-            guessed.append(usr_guess)
+            guessed += f"{usr_guess} "
             if usr_guess in word_to_be_guessed:
                 print(f"The letter {usr_guess} is in the word")
             else:
                 # logs the wrong answer
-                tot_wrong -= 1
+                tot_wrong += 1
                 print(f"The letter {usr_guess} is not in the word")
             print(tot_wrong)
             print(guessed)
-            game_board_update(guessed, tot_wrong) # needs code
+            # sends info to game board
+            game_board_update(guessed, tot_wrong, word_to_be_guessed)
         else:
             print('Input is not a letter!')
-    print('loop ended!')
+        # print(game_board)
+    game_loss(word_to_be_guessed)
     # Checks that the to see if game has been won or lost
     # asks for user input of a letter
     # sends it with criteria to validatiton function
@@ -106,10 +116,29 @@ def input_validation(usr_input, char_type):
     # back to game_running
 
 
-def game_board_update(guessed, tot_wrong):
+def game_board_update(guessed, tot_wrong, word):
     """
     Updates the hangman gameboard.
     """
+    graphic = hangmanwordbank.HANGMANPICS[tot_wrong]
+    word_progress = ""
+    for i in word:
+        if i in guessed:
+            word_progress = word_progress + f"{i} "
+        else:
+            word_progress = word_progress + "_ "
+
+    print(graphic)
+    print('=================')
+    print('Progress:')
+    print(word_progress.upper())
+    print('=================')
+    print("Previous Guesses:")
+    print(guessed.upper())
+    print('=================')
+
+    if "_" not in word_progress:
+        game_win(tot_wrong)
     # accepts input of wrongs and rights in a list
     # displays the rights in the correct index in the guessed answer
     # displays the wrong in a list next to it
@@ -117,20 +146,28 @@ def game_board_update(guessed, tot_wrong):
     # return the value to the is running
 
 
-def game_win():
+def game_win(tot_wrong):
     """
     Displays the victory message.
     """
-    # shows the general victory screen
-    # shows number of right guesses and wrong guesses below
+    print('#################')
+    print('#    Congrats!  #')
+    print('#################')
+    print("You won the game!")
+    print(f"With {tot_wrong} \nwrong guessses! ")
+    exit()
 
 
-def game_loss():
+def game_loss(word):
     """
     Displays the loss message.
     """
-    # shows the general loss screen
-    # shows number of right guesses and wrong guesses below
+    print('#################')
+    print('#    You Lost   #')
+    print('#################')
+    print("You let him hang")
+    print(f"The answer was '{word.upper()}'\nWas it really that hard?")
+    exit()
 
 
 def instructions():
@@ -142,12 +179,5 @@ def instructions():
     # what the different menu options do and commands inside the game
 
 
-def main():
-    """
-    Runs program functions and keeps track of gamestates
-    """
-    # game_menu()
-    game_running()
+game_menu()
 
-
-main()
